a9566c45dc9aa91fbd86c223e51d569bd0818ef6
carterburn <899382+carterburn@users.noreply.github.com>
2025-12-04 15:19:35 -0500

    multiple clients

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,13 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use anyhow::Result;
+use codecrafters_redis::Redis;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+const REDIS_PORT: u16 = 6379;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buf = [0u8; 4096];
-                while let Ok(n) = stream.read(&mut buf) {
-                    if n == 0 {
-                        break;
-                    }
-                    println!("{:?}", str::from_utf8(&buf[..n]));
-                    let _ = stream.write_all(b"+PONG\r\n");
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
-    }
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut redis = Redis::new(REDIS_PORT).await?;
+
+    redis.run().await?;
+
+    Ok(())
 }
